a51c50904e09f1bd9ba073857d5e130f07a81fd5
nedjmo23 <nedjmotouil23@gmail.com>
2026-08-12 15:43:39 +0100

    Add files via upload

---
+++ Common.h
@@ -0,0 +1,31 @@
+#pragma once
+
+// main.cpp - Added Reading Themes, Pin Tabs & Close Unpinned Tabs
+#include <QApplication>
+#include <QMainWindow>
+#include <QStackedWidget>
+#include <QFileDialog>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QGridLayout>
+#include <QPushButton>
+#include <QMouseEvent>
+#include <QFileInfo>
+#include <QPainter>
+#include <QLabel>
+#include <QPainterPath>
+#include <QFrame>
+#include <QMenu>
+#include <QAction>
+#include <QVector>
+#include <QPropertyAnimation>
+#include <QEasingCurve>
+#include <QSettings>
+#include <QDateTime>
+#include <QScrollArea>
+#include <QScroller>
+#include <QTimer>
+#include <windows.h>
+
+// أنماط ألوان القراءة
+enum ReadingTheme { ThemeLight, ThemeDark, ThemeSepia, ThemeNord };
